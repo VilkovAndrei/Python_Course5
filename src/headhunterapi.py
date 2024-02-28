3acabd81b60c87941eaf1e37e2ac6989a7b269dc
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-02-29 00:08:47 +0300

    Add headhunterapi.py

---
+++ src/headhunterapi.py
@@ -0,0 +1,92 @@
+import requests
+import time
+
+
+class HeadHunterAPI():
+    """Класс для работы с API HeadHunter"""
+
+    url_emp = "https://api.hh.ru/employers/"
+    url_vac = "https://api.hh.ru/vacancies/"
+
+    def __init__(self, emp_ids: list, area: int =113):
+
+        self.params_vac = {
+            "employer_id": emp_ids[0],
+            "per_page": 100,
+            "page": int,
+            "only_with_salary": True,
+            "area": area,
+            "archived": False,
+        }
+        self.vacancies = []
+        self.employers = []
+
+    def get_request_emp(self, emp_id: str):
+        response = requests.get(self.url_emp + emp_id)
+        if response.status_code != 200:
+            raise Exception(f"Ошибка получения данных работодателя {emp_id}! Статус: {response.status_code}")
+        return response.json()
+
+    def get_employers(self, emp_ids: list):
+        count_employers = 0
+        self.employers = []
+        formatted_employers = []
+        for emp_id in emp_ids:
+            try:
+                employer_data = self.get_request_emp(emp_id)
+            except Exception as error:
+                print(error)
+            else:
+                self.employers.extend(employer_data["items"])
+            finally:
+                time.sleep(1)
+
+        for employer in self.employers:
+            formatted_employer = {
+                "employer_hh_id": employer["id"],
+                "name": employer["name"],
+                "url": employer["alternate_url"],
+            }
+            formatted_employers.append(formatted_employer)
+        return formatted_employers
+
+    def get_request_vac(self):
+        response = requests.get(self.url_vac, params=self.params_vac)
+        if response.status_code != 200:
+            raise Exception(f"Ошибка получения вакансий! Статус: {response.status_code}")
+        return response.json()
+
+    def get_vacancies(self, emp_ids: list, pages_count=2):
+        count_vacancies = 0
+        self.vacancies = []
+        formatted_vacancies = []
+        for employer in self.employers:
+            self.params_vac["employer_id"] = employer["employer_hh_id"]
+            for page in range(pages_count):
+                page_vacancies = []
+                self.params_vac["page"] = page
+                #  print(f"({self.__class__.__name__}) Парсинг страницы {page} -", end=" ")
+                try:
+                    page_vacancies = self.get_request_vac()
+                except Exception as error:
+                    print(error)
+                else:
+                    self.vacancies.extend(page_vacancies["items"])
+                    print(f"Загружено вакансий: {len(page_vacancies)}")
+                    count_vacancies += len(page_vacancies)
+                finally:
+                    time.sleep(1)
+                if len(page_vacancies) == 0:
+                    break
+            #  print(f"({self.__class__.__name__}) Загружено вакансий всего: {count_vacancies}")
+            for vacancy in self.vacancies:
+                formatted_vacancy = {
+                    "employer": vacancy["employer"]["name"],
+                    "title": vacancy["name"],
+                    "url": vacancy["url"],
+                    "salary_from": vacancy["salary"]["from"] if vacancy["salary"] else None,
+                    "salary_to": vacancy["salary"]["to"] if vacancy["salary"] else None,
+                    "requirement": vacancy["snippet"]["requirement"],
+                }
+                formatted_vacancies.append(formatted_vacancy)
+        return formatted_vacancies
--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 
 from src.config import config
-
+from src.headhunterapi import HeadHunterAPI
 
 def main():
+    list_employers_id = list(
+        3529,
+        78638,
+        2748,
+        3127,
+        1740,
+        93051,
+        4219,
+        907345,
+        1471727,
+        1057047
+    )
     params_db = config()
-    pass
+    hh = HeadHunterAPI(list_employers_id)
+
 
 
 if __name__ == '__main__':
